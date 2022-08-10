90569c22cb08c9fd5af75cd59edfb6070565442e
tejas <tejasa@epiqinfo.com>
2022-08-10 11:09:35 +0000

    Updated inlfux file

---
+++ influxdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mv influx.repo /etc/yum.repos.d/influx.repo
+yum install influxdb -y && yum install telegraf -y
+systemctl enable influxdb
+systemctl start influxdb
+systemctl enable telegraf.service
+systemctl start telegraf.service
+mv telegraf.conf /etc/telegraf/telegraf.conf
+chmod 666 /var/run/docker.sock
+systemctl restart telegraf && systemctl restart influx
+
