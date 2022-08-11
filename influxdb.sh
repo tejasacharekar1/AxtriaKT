d3b4cdf7e7559be681ad8d735ff40e770458032e
tejas <tejasa@epiqinfo.com>
2022-08-11 10:23:47 +0000

    Update in influxbd file

--- influxdb.sh
+++ influxdb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash/sudo bash
-mv influx.repo /etc/yum.repos.d/influx.repo
-yum install influxdb -y && yum install telegraf -y
-systemctl enable influxdb
-systemctl start influxdb
-systemctl enable telegraf.service
-systemctl start telegraf.service
-mv telegraf.conf /etc/telegraf/telegraf.conf
-chmod 666 /var/run/docker.sock
-systemctl restart telegraf && systemctl restart influx
-
+sudo mkdir /var/lib/grafana
+sudo yum install influxdb -y && sudo yum install telegraf -y
+sudo systemctl enable influxdb
+sudo systemctl start influxdb
+sudo systemctl enable telegraf.service
+sudo systemctl start telegraf.service
+sudo mv telegraf.conf /etc/telegraf/telegraf.conf
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart telegraf && sudo systemctl restart influx
+sudo mv influx.repo /etc/yum.repos.d/influx.repo
